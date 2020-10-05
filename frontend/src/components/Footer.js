99b037ee92ff9af014b5e960ed15a2d2e924fa82
melike elma <melikapple.80@gmail.com>
2020-10-05 23:23:33 +0300

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
+export const Footer = () => {
+    return (
+        <footer>
+           <Container>
+               <Row>
+                   <Col className="text-center py-3">
+                       Copyright &copy; ProShop
+                   </Col>
+               </Row>
+               </Container> 
+        </footer>
+    )
+}
