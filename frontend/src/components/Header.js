be53c83587ca3048bed5d22259b75b5f7d48c66f
melike elma <melikapple.80@gmail.com>
2020-10-13 02:25:18 +0300

    admin-list-product

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -50,7 +50,7 @@ export const Header = () => {
                   <LinkContainer to='/admin/userlist'>
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to='/admin/productlist'>
+                  <LinkContainer to='/admin/productlist/'>
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to='/admin/orderlist'>
